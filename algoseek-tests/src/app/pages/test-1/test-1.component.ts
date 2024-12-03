747b833b6bc924b0658933d892b29d7de536774c
tolyaganzin <tolyaganzin@gmail.com>
2024-12-03 20:21:52 +0200

    add full layout set for create note

--- algoseek-tests/src/app/pages/test-1/test-1.component.ts
+++ algoseek-tests/src/app/pages/test-1/test-1.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test-1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './test-1.component.html',
   styleUrl: './test-1.component.css'
 })
 export class Test1Component {
-  user: string = "Milton Romaguera"
+  
+  user: string = "Milton Romaguera";
+  note: string = "";
+
+  types: {class: string, type: number}[] = [
+    {class: "ri-chat-1-fill", type: 0},
+    {class: "ri-phone-fill", type: 1},
+    {class: "ri-cup-fill", type: 2},
+    {class: "ri-beer-fill", type: 3},
+    {class: "ri-user-fill", type: 4}
+  ];
+  selectedType: number = 0
+  
+  handleClick(type: number): void {
+    this.selectedType = type;
+  }
+
+  submit() {
+    // add to array 
+  }
 }
